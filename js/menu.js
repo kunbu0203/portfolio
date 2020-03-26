8c1dbc2f213b635ab9eca6482ebf9883d8087d72
彭立璇 <vivi@design-edg.com>
2020-03-26 22:23:22 +0800

    fix

--- js/menu.js
+++ js/menu.js
@@ -1,64 +1,9 @@
 $(document).ready(function () {
-    $('.ham').click(function (e) { 
+    $('.menuBtn').click(function (e) { 
         e.preventDefault();
-        $('header').toggleClass('show');
+        $('.main').toggleClass('itemHide');
+        $('.main').toggleClass('menuOpen');
     });
-    $('.getStart').click(function (e) { 
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $('section.contect .wrapper').offset().top
-        }, 800);
-        $('header').removeClass('show');
-    });
-    $('.showPopup').click(function (e) { 
-        e.preventDefault();
-        $('#activePopup').show();
-    });
-    $('#btnWinnerPopup').click(function (e) { 
-      e.preventDefault();
-      $('#winnerPopup').show();
-    });
-
-
-    var swiper = new Swiper('.swiper-container', {
-        effect: 'coverflow',
-        slidesPerView: 1.2,
-        spaceBetween: -100,
-        centeredSlides: true,
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        coverflowEffect: {
-            rotate: 0,
-            stretch: 0,
-            depth: 500,
-            slideShadows : false,
-        },
-        breakpoints: {
-            // when window width is >= 320px
-            1025: {
-            slidesPerView: 2,
-            }
-        },
-        on: {
-            slideChangeTransitionStart: function () {
-            $('.camera').removeClass('active');
-            },
-            slideChangeTransitionEnd: function () {
-            $('.camera').addClass('active');
-            }
-        }
-    });
-
-    doAnimate();
-    
-    GAtracking();
-
-
 });
+
+
