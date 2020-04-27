54146ac10324c170e25e7873990dcccc92bc7bf5
彭立璇 <kunbu0203@gmail.com>
2020-04-27 22:52:03 +0800

    fix

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,9 +1,10 @@
+
 $(document).ready(function () {
 	//initialize swiper when document ready
 	var mySwiper = new Swiper ('.swiper-container', {
 		// Optional parameters
 		effect: 'coverflow',
-		slidesPerView: 3.38,
+		slidesPerView: 1.9,
 		// grabCursor: true,
 		centeredSlides: true,
 		spaceBetween: 0,
@@ -11,12 +12,23 @@ $(document).ready(function () {
 		coverflowEffect: {
 			rotate: 0,
 			stretch: 0,
-			depth: 250,
+			depth: 500,
 			slideShadows : false,
 		},
 		autoplay: {
             delay: 2000,
             disableOnInteraction: false,
+		},
+		breakpoints: {
+            1041: {
+			slidesPerView: 3.38,
+			coverflowEffect: {
+				depth: 250,
+				},
+            }
         },
 	})
+
+	// doAnimate();
+
 });
